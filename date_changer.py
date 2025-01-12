ba58830934ea53851ac34d212a5405c3054f57af
Ольга <ola2306@yandex.ru>
2025-01-12 21:18:59 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,21 @@
+# Работа с датами
+# Первичный формат дат
+
+created_date = '10-01-2025'
+issue_date = '20-01-2025'
+
+# Промежуточные переменные с урезанным форматом даты
+temp_created_date = created_date[0:5]
+temp_issue_date = issue_date[0:5]
+
+# Вывод на печать урезанных дат
+print(temp_issue_date)
+print(temp_created_date)
+
+print(created_date[0:5])
+print(issue_date[0:5])
+
+# Проверка сохранения полного формата дат первоначальных переменных
+print(created_date)
+print(issue_date)
+
