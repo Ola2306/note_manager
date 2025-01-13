552c01cf980a9392b650c6405b582f2bffa8f255
Ольга <ola2306@yandex.ru>
2025-01-13 11:57:55 +0300

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,16 @@
+# Grade 1. Этап 1. Задание 4
+# Переменные для заголовков
+
+title1 = input("Заголовок заметки: ")
+title2 = input("Заголовок заметки: ")
+title3 = input("Заголовок заметки: ")
+
+titles = [title1, title2, title3]
+
+# Печать переменных и списка
+
+print(title1)
+print(title2)
+print(title3)
+
+print(titles)
